0916350c631f1ef46052b6cb583c19c3a4383414
Walker <c.walker@cwalker-lt.skatelescope.org>
2019-07-02 11:12:46 +0100

    writing code

--- ParkesCoincidencer_Destroy.py
+++ ParkesCoincidencer_Destroy.py
@@ -8,8 +8,18 @@ from matplotlib import pyplot as plt
 #folder with destroy .pls candidate files to test
 folder = '/share/nas1/cwalker/SKA_work/SMC_search_dev/test_files/LorimerBurst/SMC021_008_results/'
 
-#list destroy files
+#list destroy candidate files
 allfiles = os.listdir(folder)
 candfiles = [i for i in allfiles if i[-4:]=='.pls']
 
-print candfiles
+#destroy candidate files are named with convention: SMCXXX_YYBB_DM.pls
+#where XXX_YY refers to pointing (e.g. 021_008)
+#BB refers to beam (B1=11, B2=21, ... BD=D1)
+#DM is the dispersion measure (e.g. 631.0)
+
+#sort candidates into sublist containing all beams for each pointing, DM
+
+print candfiles[0:9]
+print np.unique(candfiles[0:9])
+
+

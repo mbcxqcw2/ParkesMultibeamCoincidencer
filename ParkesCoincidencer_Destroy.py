0ade00a7c63d4d7aa303246d4cbf7dd6aa5be3e5
Walker <c.walker@cwalker-lt.skatelescope.org>
2019-07-16 12:04:37 +0100

    writing code

--- ParkesCoincidencer_Destroy.py
+++ ParkesCoincidencer_Destroy.py
@@ -65,7 +65,7 @@ def GenBeamID(DestroyFileName):
     beamIDs     = ['B1','B2','B3','B4','B5','B6','B7','B8','B9','BA','BB','BC','BD'] #Beam IDs according to my naming convention
 
     beamNumber = DestroyFileName.split('_')[1][-2:] #extract beam number
-    print 'BEAMNUMBER:{0}'.format(beamNumber),beamnumbers,np.where(beamnumbers=='61')
+    print 'BEAMNUMBER:{0}'.format(beamNumber),beamnumbers,np.where(np.array(beamnumbers)==beamNumber)
     BeamID = np.where(beamnumbers==beamNumber)
     print 'SEARCH',BeamID
     return BeamID
